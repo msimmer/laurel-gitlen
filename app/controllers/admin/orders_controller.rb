e37d13fd1ddb0e2c89f4b11a14c3ebcc5b135f9a
dally <trill.dilla@gmail.com>
2013-12-11 21:48:43 -0800

    stuff

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,9 +10,9 @@ class Admin::OrdersController < ApplicationController
     @exhibition = Exhibition.find(params[:exhibition_id])
     unless params[:order].blank? || params[:order_string].blank?
       if params[:order] == 'works'
-        @exhibition.piece_order = params[:order_string].split(",")
+        @exhibition.piece_order = params[:order_string].split(",").map{ |s| s.to_i }
       elsif params[:order] == 'artists'
-        @exhibition.artist_order = params[:order_string].split(",")
+        @exhibition.artist_order = params[:order_string].split(",").map{ |s| s.to_i }
       end
       @exhibition.save
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def date_markup(exhibition)
+    if exhibition.begins.year == exhibition.ends.year
+      "#{exhibition.begins.strftime('%m.%d')}&mdash;<br/>#{exhibition.ends.strftime('%m.%d.%y')}"
+    else
+      "#{exhibition.begins.strftime('%m.%d.%y')}&mdash;<br/>#{exhibition.ends.strftime('%m.%d.%y')}"
+    end
+  end
 end
