c75953409fa2bb865da340b8b2dcf49f287f4b2e
dally <trill.dilla@gmail.com>
2013-12-13 15:32:35 -0500

    eager load

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,6 +1,6 @@
 class ExhibitionsController < ApplicationController
   def index
-    @exhibitions = Exhibition.all(order: "begins DESC")
+    @exhibitions = Exhibition.includes(:pieces).order("begins DESC")
     @exhibitions_by_year = @exhibitions.group_by{ |e| e.begins.year }
   end
   
