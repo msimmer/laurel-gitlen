61198b5df00ae094a52ef450cd38b58932f76faf
dally <trill.dilla@gmail.com>
2014-01-20 10:53:33 -0500

    gallery header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def date_markup(exhibition)
+  def date_markup(exhibition, line_break=false)
     if exhibition.begins.year == exhibition.ends.year
-      "#{exhibition.begins.strftime('%m.%d')}&mdash;<br/>#{exhibition.ends.strftime('%m.%d.%y')}"
+      "#{exhibition.begins.strftime('%m.%d')}&mdash;#{'<br/>' if line_break}#{exhibition.ends.strftime('%m.%d.%y')}"
     else
-      "#{exhibition.begins.strftime('%m.%d.%y')}&mdash;<br/>#{exhibition.ends.strftime('%m.%d.%y')}"
+      "#{exhibition.begins.strftime('%m.%d.%y')}&mdash;#{'<br/>' if line_break}#{exhibition.ends.strftime('%m.%d.%y')}"
     end
   end
 end
