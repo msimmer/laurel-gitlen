fca291f079a811c7bf0f46a9f7f5e5e85ce135f6
dally <trill.dilla@gmail.com>
2014-03-21 10:18:42 -0400

    hard code corwin

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def splash
-    @exhibition = Exhibition.where("begins <= ?", Date.today).order("begins DESC").first
+    @exhibition = Exhibition.find(35)
   end
   
   def contact
