281dffef534643ea286eb2c37fed31047ee808b1
dally <trill.dilla@gmail.com>
2014-06-17 14:03:35 -0400

    remove extra artist select

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,6 +1,6 @@
 class ExhibitionsController < ApplicationController
   def index
-    @exhibitions = Exhibition.includes(:pieces, :artists).order("begins DESC")
+    @exhibitions = Exhibition.includes(:pieces, :artists).order("begins DESC").load
     @exhibitions_by_year = @exhibitions.group_by{ |e| e.begins.year }
   end
   
